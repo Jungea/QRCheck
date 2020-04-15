c81b6d1e16c68536cac574241ed5d138c072f7d3
Jungea <jungea2241@gmail.com>
2020-04-15 23:19:10 +0900

    웹에서 관리자/ 학생 기능 제거

--- src/main/java/net/skhu/controller/CommonController.java
+++ src/main/java/net/skhu/controller/CommonController.java
@@ -20,7 +20,7 @@ import net.skhu.repository.StudentRepository;
 
 @Controller
 public class CommonController {
-	
+
 	@Autowired
 	AdminRepository adminRepository;
 	@Autowired
@@ -30,39 +30,38 @@ public class CommonController {
 	@Autowired
 	RegistrationRepository registrationRepository;
 
-	
 	@RequestMapping(value = "login", method = RequestMethod.GET)
 	public String login(Model model) {
 		return "login";
 	}
- 
+
 	// 로그인 화면에서 로그인버튼 클릭
 	@RequestMapping(value = "login", method = RequestMethod.POST)
-	public String login(Model model,  @RequestParam("num") String num, @RequestParam("pass") String pass, HttpServletRequest request) {
+	public String login(Model model, @RequestParam("num") String num, @RequestParam("pass") String pass,
+			HttpServletRequest request) {
 		HttpSession session = request.getSession();
 		System.out.println(num + " " + pass);
-		
-		if(adminRepository.findByAdminNumAndPassword(num, pass) != null) {
-			System.out.println("관리자");
-			session.setAttribute("userNum", num);
-			return "redirect:professor/list";
-		} else if(professorRepository.findByProfNumAndPassword(num, pass)!= null){
+
+//		if(adminRepository.findByAdminNumAndPassword(num, pass) != null) {
+//			System.out.println("관리자");
+//			session.setAttribute("userNum", num);
+//			return "redirect:professor/list";
+//		} else 
+		if (professorRepository.findByProfNumAndPassword(num, pass) != null) {
 			System.out.println("교수");
 			session.setAttribute("userNum", num);
 			return "redirect:professor/list";
-		}else if(studentRepository.findByStuNumAndPassword(num, pass)!=null) {
+		} else if (studentRepository.findByStuNumAndPassword(num, pass) != null) {
 			System.out.println("학생");
-			session.setAttribute("userNum", num);
-			return "redirect:professor/list";	
-		}else {
+			model.addAttribute("message", "[ 학생은 출석 체크 어플을 이용해주시기 바랍니다. ]");
+			return "login";
+		} else {
 			System.out.println("잘못된 입력");
 			model.addAttribute("message", "[ 로그인 정보가 일치하지 않습니다. ]");
 			return "login";
 		}
 	}
-	
 
-	
 	@RequestMapping("insertRegi/{stuId}/{courId}/{divId}")
 	public Student student(@PathVariable("id") int id) {
 		return studentRepository.findById(id).get();
