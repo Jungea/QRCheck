fd252ca08505c962121ab2461ffe3bec602c01f6
Jungea <jungea2241@gmail.com>
2020-04-15 12:37:27 +0900

    학생의 수강 강의목록

--- src/main/java/net/skhu/controller/StudentController.java
+++ src/main/java/net/skhu/controller/StudentController.java
@@ -8,11 +8,14 @@ import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import net.skhu.domain.Registration;
 import net.skhu.domain.Student;
-import net.skhu.repository.DepartmentRepository;
+import net.skhu.repository.AdminRepository;
+import net.skhu.repository.RegistrationRepository;
 import net.skhu.repository.StudentRepository;
 
 @Controller
@@ -22,47 +25,38 @@ public class StudentController {
 	@Autowired
 	StudentRepository studentRepository;
 	@Autowired
-	DepartmentRepository departmentRepository;
+	AdminRepository adminRepository;
+	@Autowired
+	RegistrationRepository registrationRepository;
 
-	@RequestMapping(value = "login", method = RequestMethod.GET)
-	public String login(Model model) {
-		Student Student = new Student();
-		model.addAttribute("Student", Student);
-		return "login/login";
+	public int getSemId() {
+		return adminRepository.findById(1).get().getSemester().getId();
 	}
-//
-//	// 로그인 화면에서 로그인버튼 클릭
-//	@RequestMapping(value = "login", method = RequestMethod.POST)
-//	public String login(Model model, Student Student, HttpServletRequest request) {
-//		System.out.println(Student.getStudentId() + " " + Student.getStudentPass());
-//		Student u = StudentRepository.findByStudentIdAndStudentPass(Student.getStudentId(), Student.getStudentPass()).get(0);
-//		if (u != null) {
-//			HttpSession session = request.getSession();
-//			session.setAttribute("Student", u);
-//			System.out.println(u);
-//			return "redirect:list";
-//		} else
-//			System.out.println(u);
-//		return "login/login";
-//	}
-//
-//	// 페이지 내의 로그아웃 버튼을 클릭
-//	@RequestMapping(value = "logout")
-//	public String logout(Model model, HttpServletRequest request) {
-//		HttpSession session = request.getSession();
-//		session.removeAttribute("Student");
-//		return "redirect:login";
-//	}
+	
+	// 페이지 내의 로그아웃 버튼을 클릭
+	@RequestMapping(value = "logout")
+	public String logout(Model model, HttpServletRequest request) {
+		HttpSession session = request.getSession();
+		session.removeAttribute("userNum");
+		return "redirect:/login";
+	}
 
-	@RequestMapping("list")
-	public String list(Model model) {
-		List<Student> list = studentRepository.findAll();
+	@RequestMapping("list/{stuId}")
+	public String list(Model model, @PathVariable("stuId") int stuId) {
+		List<Registration> list = registrationRepository.findByStudent_IdAndCourse_Semester_Id(stuId, getSemId());
 		model.addAttribute("list", list);
 		return "student/list";
 	}
 	
 	@RequestMapping("check")
 	public String check(Model model, int studentId, int courseId) {
+		List<Student> list = studentRepository.findAll();
+		model.addAttribute("list", list);
+		return "student/list";
+	}
+	
+	@RequestMapping("insertRegistration")
+	public String insertRegistration(Model model, int studentId, int courseId) {
 		List<Student> list = studentRepository.findAll();
 		model.addAttribute("list", list);
 		return "student/list";
--- src/main/webapp/WEB-INF/views/student/list.jsp
+++ src/main/webapp/WEB-INF/views/student/list.jsp
@@ -1,47 +1,57 @@
-<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
-<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
 <c:url var="R" value="/" />
 <!DOCTYPE html>
 <html>
 <head>
-  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-  <link href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" 
-        rel="stylesheet" media="screen">
-  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-  <script src="${R}res/common.js"></script>
-  <link rel="stylesheet" href="${R}res/common.css">
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<link rel="stylesheet"
+	href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+	integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
+	crossorigin="anonymous">
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
+	integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n"
+	crossorigin="anonymous"></script>
+<script
+	src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
+	integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6"
+	crossorigin="anonymous"></script>
+<script src="${R}res/common.js"></script>
+
 </head>
 <body>
-<div class="container">
-  <h1>학생목록</h1>
-  <div class="pull-right mb5">
-  <a href="create" class="btn btn-info">
-    <span class="glyphicon glyphicon-user"></span> 학생등록</a>
-  </div>    
-  <table class="table table-bordered mt5">
-    <thead>
-      <tr>
-        <th>id</th>
-        <th>학번</th>
-        <th>이름</th>
-        <th>학과</th>
-        <th>학년</th>
-      </tr>
-    </thead>
-    <tbody>
-      <c:forEach var="student" items="${ list }">
-        <tr data-url="edit?id=${ student.id }">
-          <td>${ student.id }</td>
-          <td>${ student.studentNo }</td>
-          <td>${ student.name }</td>
-          <td>${ student.department.name }</td>
-          <td>${ student.year }</td>
-        </tr>
-      </c:forEach>
-    </tbody>
-  </table>
-</div>
+	<div class="container">
+		<h1>학생목록</h1>
+		
+		<hr>
+		
+		<table class="table table-bordered mt5">
+			<thead>
+				<tr>
+					<th>regiId</th>
+					<th>강의명</th>
+					<th>강의코드</th>
+					<th>강의시간</th>
+					<th>강의실</th>
+				</tr>
+			</thead>
+			<tbody>
+				<c:forEach var="registration" items="${ list }">
+					<tr data-url="edit?id=${ registration.id }">
+						<td>${ registration.id }</td>
+						<td>${ registration.course.name }</td>
+						<td>${ registration.division.code }</td>
+						<td><c:forEach var="time"
+								items="${ registration.course.times }">
+							${ time.getStringWeek(time.dayOfWeek) } ${ time.startTime } ~ ${ time.endTime }
+						</c:forEach></td>
+						<td>${ registration.course.room.code }</td>
+					</tr>
+				</c:forEach>
+			</tbody>
+		</table>
+	</div>
 </body>
 </html>
 
